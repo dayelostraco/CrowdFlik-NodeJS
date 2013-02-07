37ee11490bd8eed77febcf359d6b1927e042f8bc
Dayel Ostraco <dayel.ostraco@gmail.com>
2013-02-07 13:30:12 -0500

    Updated startup messages

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('/event/:latitude/:longitude/', event.findEventNearYou);
 app.post('/event/', event.createEvent);
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+    console.log("Express is server listening on port " + app.get('port'));
 });
